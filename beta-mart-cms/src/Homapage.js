bbb4dcff5a800d90ee9d07a517502b7135dee007
Abriam Michaela Joan <abriammichaelajoan36@gmail.com>
2024-05-25 08:35:52 -0800

    Create Homepage.js

---
+++ beta-mart-cms/src/Homapage.js
@@ -0,0 +1,15 @@
+Homepage.js
+
+import React from 'react';
+import ProductList from '../components/ProductList';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default HomePage;
