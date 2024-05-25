a68bb7672e174d16d98c70db67cbcf55aefbfd63
azraqt <altoverosdarren13@gmail.com>
2024-05-25 08:28:33 +0800

    Update App.js

--- beta-mart-cms/src/App.js
+++ beta-mart-cms/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AddProductPage from './pages/AddProductPage';
+import EditProductPage from './pages/EditProductPage';
+import UserManagementPage from './pages/UserManagementPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/add-product" component={AddProductPage} />
+        <Route path="/edit-product/:id" component={EditProductPage} />
+        <Route path="/manage-users" component={UserManagementPage} />
+      </Switch>
+    </Router>
   );
 }
 
