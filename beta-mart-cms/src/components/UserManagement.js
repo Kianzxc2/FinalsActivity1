66347112315755fe381b09fb057c827687b1486b
ArielAmante <amanteariel660@gmail.com>
2024-05-25 09:18:16 +0800

    create UserManagement.js / update Homepage.js

---
+++ beta-mart-cms/src/components/UserManagement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserManagement() {
+  return (
+    <div>
+      <h1>User Management</h1>
+      <p>Manage your users here.</p>
+    </div>
+  );
+}
+
+export default UserManagement;
--- beta-mart-cms/src/pages/HomePage.js
+++ beta-mart-cms/src/pages/HomePage.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ProductList from '../components/ProductList';
 
 function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <ProductList />
+      <p>Welcome to the home page!</p>
     </div>
   );
 }
